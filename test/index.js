f554081bc44257e9204ef1cc801f2ac5f2f24b99
haoxin <coderhaoxin@outlook.com>
2015-06-04 13:34:27 +0800

    more tests

--- test/index.js
+++ test/index.js
@@ -1,43 +1,88 @@
 'use strict';
 
-let assert = require('assert');
-let pg = require('..');
+const assert = require('assert');
+const pg = require('..');
 
 const config = 'postgres://hx@localhost/hx';
 
 describe('## pg-then', function() {
   describe('# Pool', function() {
-    let pool;
+    let pool = pg.Pool(config);
 
-    it('new pool', function(done) {
-      pool = pg.Pool(config);
-
-      setTimeout(done, 1000);
+    it('query', function() {
+      return pool.query('SELECT 1 AS count')
+        .then(function(result) {
+          assert.equal(result.rowCount, 1);
+          assert.equal(result.rows[0].count, 1);
+        });
     });
 
     it('query', function() {
-      return pool.query('SELECT 1 AS count').then(function(result) {
-        assert.equal(result.rowCount, 1);
-        assert.equal(result.rows[0].count, 1);
-      });
+      return pool.query('SELECT 1 AS count')
+        .then(function(result) {
+          assert.equal(result.rowCount, 1);
+          assert.equal(result.rows[0].count, 1);
+        });
     });
   });
 
   describe('# Client', function() {
-    let client;
+    let client = pg.Client(config);
 
-    it('new client', function(done) {
-      client = pg.Client(config);
-
-      setTimeout(done, 1000);
+    it('query', function() {
+      return client.query('SELECT 1 AS count')
+        .then(function(result) {
+          assert.equal(result.rowCount, 1);
+          assert.equal(result.rows[0].count, 1);
+          client.end();
+        });
     });
 
     it('query', function() {
-      return client.query('SELECT 1 AS count').then(function(result) {
-        assert.equal(result.rowCount, 1);
-        assert.equal(result.rows[0].count, 1);
-        client.end();
-      });
+      return client.query('SELECT 1 AS count')
+        .then(function(result) {
+          assert.equal(result.rowCount, 1);
+          assert.equal(result.rows[0].count, 1);
+          client.end();
+        });
+    });
+  });
+
+  describe('# error', function() {
+    it('pool connect error', function(done) {
+      pg.Pool('postgres://xx@localhost/xx')
+        .query('SELECT 1 AS count')
+        .catch(function(error) {
+          assert.ok(error instanceof Error);
+          done();
+        });
+    });
+
+    it('client connect error', function(done) {
+      pg.Client('postgres://xx@localhost/xx')
+        .query('SELECT 1 AS count')
+        .catch(function(error) {
+          assert.ok(error instanceof Error);
+          done();
+        });
+    });
+
+    it('pool query error', function(done) {
+      pg.Pool(config)
+        .query('SYNTAX ERROR')
+        .catch(function(error) {
+          assert.ok(error instanceof Error);
+          done();
+        });
+    });
+
+    it('client query error', function(done) {
+      pg.Client(config)
+        .query('SYNTAX ERROR')
+        .catch(function(error) {
+          assert.ok(error instanceof Error);
+          done();
+        });
     });
   });
 });
